9336f941f74184ee1efe20f952ebf894a79d2103
minsu <minsu.kim@khmc.or.kr>
2018-08-21 13:21:53 +0900

    클래스 사용

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
 /*Typed : 어떤 종류의 변수와 데이터인지 설정 혹은 예상
-함수뒤에 String 값을 넣고 return을 해주면 문자열을 반환함*/
-interface Human{
-        name: string;
-        age: number;
-        gender:string;
+함수뒤에 String 값을 넣고 return을 해주면 문자열을 반환함
+인터페이스는 js로 컴파일 되진 않음.
+그래서 사용하는것이 class*/
+class Human{
+        public name: string;
+        public age: number;
+        public gender: string;
+        constructor(name:string, age:number, gender:string){
+                this.name = name;
+                this.age = age;
+                this.gender = gender;
+        }
 }
-const student={
-        name:"minsu",
-        age:28,
-        gender:"male"
-}
+
+const author = new Human("Minsu",22,"male")
 
 const sayHi = (people:Human):string=>{
         return `Hello ${people.name}, you are ${people.age}, you are a ${people.gender}` ;
 
 };
 
-console.log(sayHi(student));
+console.log(sayHi(author));
 
 export{};

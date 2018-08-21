7a41545aab8bffbc95a8b2e01c881671cd6191c2
minsu <minsu.kim@khmc.or.kr>
2018-08-21 11:54:36 +0900

    Object 사용하기

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,12 @@
 "use strict";
-/*Typed : 어떤 종류의 변수와 데이터인지 설정 혹은 예상
-함수뒤에 String 값을 넣고 return을 해주면 문자열을 반환함*/
 Object.defineProperty(exports, "__esModule", { value: true });
-var sayHi = function (name, age, gender) {
-    return "Hello " + name + ", you are " + age + ", you are a " + gender;
+const student = {
+    name: "minsu",
+    age: 28,
+    gender: "male"
 };
-console.log(sayHi("minsu", 28, "Male"));
+const sayHi = (people) => {
+    return `Hello ${people.name}, you are ${people.age}, you are a ${people.gender}`;
+};
+console.log(sayHi(student));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 /*Typed : 어떤 종류의 변수와 데이터인지 설정 혹은 예상
 함수뒤에 String 값을 넣고 return을 해주면 문자열을 반환함*/
+interface Human{
+        name: string;
+        age: number;
+        gender:string;
+}
+const student={
+        name:"minsu",
+        age:28,
+        gender:"male"
+}
 
-
-const sayHi = (name:string,age:number,gender:string):string=>{
-        return `Hello ${name}, you are ${age}, you are a ${gender}` ;
+const sayHi = (people:Human):string=>{
+        return `Hello ${people.name}, you are ${people.age}, you are a ${people.gender}` ;
 
 };
 
-console.log(sayHi("minsu",28,"Male"));
+console.log(sayHi(student));
 
 export{};

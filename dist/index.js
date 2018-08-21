baa9875d0d66b5dec94c0ee94ec1ee0c67a10740
minsu <minsu.kim@khmc.or.kr>
2018-08-21 16:04:04 +0900

    First 블록 생성 Type : 클래스 , Typescript

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-/*Typed : 어떤 종류의 변수와 데이터인지 설정 혹은 예상
-함수뒤에 String 값을 넣고 return을 해주면 문자열을 반환함
-인터페이스는 js로 컴파일 되진 않음.
-그래서 사용하는것이 class*/
-class Human {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    constructor(index, hash, previousHash, data, timestemp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestemp = timestemp;
     }
 }
-const author = new Human("Minsu", 22, "male");
-const sayHi = (people) => {
-    return `Hello ${people.name}, you are ${people.age}, you are a ${people.gender}`;
-};
-console.log(sayHi(author));
+const geneisBlock = new Block(0, "202020202", "", "Hello", 123456);
+let blockChain = [geneisBlock];
+console.log(blockChain);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
-/*Typed : 어떤 종류의 변수와 데이터인지 설정 혹은 예상
-함수뒤에 String 값을 넣고 return을 해주면 문자열을 반환함
-인터페이스는 js로 컴파일 되진 않음.
-그래서 사용하는것이 class*/
-class Human{
-        public name: string;
-        public age: number;
-        public gender: string;
-        constructor(name:string, age:number, gender:string){
-                this.name = name;
-                this.age = age;
-                this.gender = gender;
+
+class Block{
+        public index:number;
+        public hash: string;
+        public previousHash: string;
+        public data: string;
+        public timestemp: number;
+        constructor(index:number, hash:string, previousHash:string, data:string,timestemp:number){
+                this.index = index;
+                this.hash = hash;
+                this.previousHash = previousHash;
+                this.data = data;
+                this.timestemp = timestemp;
+
         }
+
+        
+
 }
+const geneisBlock: Block = new Block(0, "202020202","","Hello",123456);
 
-const author = new Human("Minsu",22,"male")
+let blockChain: [Block] = [geneisBlock];
 
-const sayHi = (people:Human):string=>{
-        return `Hello ${people.name}, you are ${people.age}, you are a ${people.gender}` ;
-
-};
-
-console.log(sayHi(author));
-
+console.log(blockChain);
 export{};
